462afd52d26a713d69577f3858d4f0261cab7896
Muhammad Hamza Khan <m.hamzakhan91able@gmail.com>
2019-07-04 16:27:03 +0500

    Added Current and Historic Stats in Dashboard

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -4,6 +4,11 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
 
 const styles = theme => ({
     root: {
@@ -20,16 +25,140 @@ const styles = theme => ({
     },
     toolbar: {
         color: 'black'
-    }
+    },
+    grid: {
+        marginTop: 20,
+        width: "unset",
+        flexShrink: 0,
+    },
+    card: {
+        minWidth: 200,
+        height: 250
+    },
+    roundCircle: {
+        border: '0.1em solid #32CD32',
+        borderRadius: 60,
+        height: 100,
+        width: 100,
+        textAlign: 'center',
+        background: '#32CD32',
+        marginLeft: theme.spacing(2),
+        marginRight: theme.spacing(2),
+    },
+    r2: {
+        marginTop: theme.spacing(2.5),
+        fontSize: 40,
+        fontWeight: 'bold',
+        fontFamily: 'sans - serif',
+        color: 'white',
+    },
+    rowC: {
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    heading: {
+        textAlign: 'center',
+        marginTop: 10,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
 });
 
 class Dashboard extends React.Component {
+
+    renderCurrent = () => {
+        const { classes } = this.props;
+
+        return (
+            <React.Fragment>
+                <Grid container xs={12} sm={8} className={classes.grid}>
+                    <Card className={classes.card}>
+                        <Typography className={classes.heading} variant="h5" component="h2">
+                            Current
+                        </Typography>
+                        <CardContent className={classes.rowC}>
+                            <Typography className={classes.roundCircle}>
+                                <Typography className={classes.r2}>
+                                    12
+                                </Typography>
+                                <Button color='primary' style={{ marginTop: 30 }}>
+                                    Filled Shifts
+                                </Button>
+                            </Typography>
+                            <Typography className={classes.roundCircle} style={{ border: '0.1em solid #CCCC00', background: '#CCCC00' }}>
+                                <Typography className={classes.r2}>
+                                    5
+                                </Typography>
+                                <Button color='primary' style={{ marginTop: 30 }}>
+                                    Vacant Shifts
+                                </Button>
+                            </Typography>
+                            <Typography className={classes.roundCircle} style={{ border: '0.1em solid #CD5C5C', background: '#CD5C5C' }}>
+                                <Typography className={classes.r2}>
+                                    3
+                                </Typography>
+                                <Button color='primary' style={{ marginTop: 30 }}>
+                                    Unfilled Shifts
+                                </Button>
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </React.Fragment>
+        )
+    }
+
+    renderHistoric = () => {
+        const { classes } = this.props;
+
+        return (
+            <React.Fragment>
+                <Grid container xs={12} sm={8} className={classes.grid}>
+                    <Card className={classes.card}>
+                        <Typography className={classes.heading} variant="h5" component="h2">
+                            Historic
+                        </Typography>
+                        <CardContent className={classes.rowC}>
+                            <Typography className={classes.roundCircle}>
+                                <Typography className={classes.r2}>
+                                    147
+                                </Typography>
+                                <Button color='primary' style={{ marginTop: 30 }}>
+                                    Vacant Shifts
+                                </Button>
+                            </Typography>
+                            <Typography className={classes.roundCircle} style={{ border: '0.1em solid #CCCC00', background: '#CCCC00' }}>
+                                <Typography className={classes.r2}>
+                                    136
+                                </Typography>
+                                <Button color='primary' style={{ marginTop: 30 }}>
+                                    Filled Shifts
+                                </Button>
+                            </Typography>
+                            <Typography className={classes.roundCircle} style={{ border: '0.1em solid #CD5C5C', background: '#CD5C5C' }}>
+                                <Typography className={classes.r2}>
+                                    9
+                                </Typography>
+                                <Button color='primary' style={{ marginTop: 30 }}>
+                                    Unfilled Shifts
+                                </Button>
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </React.Fragment>
+        )
+    }
 
     render() {
         const { classes } = this.props;
 
         return (
             <div className={classes.root}>
+
                 <AppBar position="static" className={classes.appBar}>
                     <Toolbar className={classes.toolbar}>
                         <Typography variant="h6" className={classes.title}>
@@ -38,6 +167,20 @@ class Dashboard extends React.Component {
                         <Button color="secondary" variant="contained">Post or manage jobs</Button>
                     </Toolbar>
                 </AppBar>
+
+                <Grid container spacing={3}>
+                    <Grid container justify="center" xs={12} sm={6}>
+                        {this.renderCurrent()}
+                    </Grid>
+                    <Grid container justify="center" xs={12} sm={6}>
+                        {this.renderHistoric()}
+                    </Grid>
+                </Grid>
+
+                <Grid item xs={12}>
+                    <Paper className={classes.paper}>xs=12</Paper>
+                </Grid>
+
             </div>
         );
     }
